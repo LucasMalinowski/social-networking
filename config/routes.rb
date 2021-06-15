1700f5407408e8ad835bdb73a566a30202aa1aa9
LucasBrazau <lucas.malinowski@alfaconcursos.com.br>
2021-06-15 15:09:50 -0300

    admin sign in and sign out

---
+++ config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+  devise_for :admins, skip: [:registrations]#, controllers: (sessions: 'admins/sessions')
+
+  namespace :admin do 
+    root to: "home#index"
+  end
+ 
+  root to: "home#index"
+end
