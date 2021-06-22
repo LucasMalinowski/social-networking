f342184c6479f6ccf348e3fd8ed6bee21157d08a
LucasBrazau <lucas.malinowski@alfaconcursos.com.br>
2021-06-22 15:36:40 -0300

    Test follow

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,4 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
@@ -8,4 +6,10 @@ class User < ApplicationRecord
 
   has_one_attached :avatar
   has_many :posts
+
+  has_many :followers_subscriptions, foreign_key: :followed_id, class_name: 'Subscription'
+  has_many :followers, through: :followers_subscriptions, source: :followed_by
+
+  has_many :following_subscriptions, foreign_key: :followed_by_id, class_name: 'Subscription'
+  has_many :following, through: :following_subscriptions, source: :followed
 end 
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :user do
     name {"Luciano"}
-    email {"user@user.com"}
+    email {"user#{rand(1..1000)}@user.com"}
     password {"password"}
     phone {"90909090"}
     avatar {Rack::Test::UploadedFile.new(Rails.root.join("spec", "support", "test-avatar.png"), "image/png")}
